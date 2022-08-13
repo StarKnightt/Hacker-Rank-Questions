12d509eabd17355c4f107b0300cb53452f4284eb
StarKnight <92244026+StarKnightt@users.noreply.github.com>
2022-08-13 20:14:06 +0530

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+// Hacker rank question 1
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
+
+// Have a nice day
