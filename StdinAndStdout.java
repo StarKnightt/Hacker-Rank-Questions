579821dd5dad36c8fc022174baff38b142441f97
StarKnight <92244026+StarKnightt@users.noreply.github.com>
2022-08-14 15:21:40 +0530

    Create StdinAndStdout.java

---
+++ StdinAndStdout.java
@@ -0,0 +1,16 @@
+// Hacker rank question 2.
+
+import java.util.Scanner;
+public class Solution {
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+int num1 = sc.nextInt();
+int num2 = sc.nextInt();
+int num3 = sc.nextInt();
+System.out.println(num1);
+System.out.println(num2);
+System.out.println(num3);
+    }
+}
+
+// Have a wondeful day.
