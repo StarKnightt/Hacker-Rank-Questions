bad4d89f2f5f96a1340c68db6bcc8351fa3e412f
StarKnight <92244026+StarKnightt@users.noreply.github.com>
2022-08-14 15:24:20 +0530

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1,8 +1,9 @@
 import java.io.*;
 import java.util.*;
-// Hacker rank question 1
-public class Solution {
 
+// Hacker rank question 1
+
+public class Solution {
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         System.out.println("Hello, World.");
