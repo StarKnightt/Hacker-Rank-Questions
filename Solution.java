d12a99e97faa037621510d7265f7216704d1d7dc
StarKnight <92244026+StarKnightt@users.noreply.github.com>
2022-08-18 13:08:01 +0530

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -2,7 +2,6 @@ import java.io.*;
 import java.util.*;
 
 // Hacker rank question 1
-
 public class Solution {
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
