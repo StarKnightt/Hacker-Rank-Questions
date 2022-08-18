def7fe14470ce8d6f0e62e7351ea1ea5be2686cb
StarKnight <92244026+StarKnightt@users.noreply.github.com>
2022-08-18 13:10:04 +0530

    Create Java Loops I.java

---
+++ Java Loops I.java
@@ -0,0 +1,13 @@
+// Using Java 15
+
+import java.util.Scanner;
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("%d x %d = %d\n", n, i, n*i);
+        }
+    }
+}
+// Have a amazing day.
