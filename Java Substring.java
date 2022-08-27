3b564c077ef3cf6474b5929df97e96f89b3e1680
StarKnight <92244026+StarKnightt@users.noreply.github.com>
2022-08-27 12:21:04 +0530

    Create Java Substring.java

---
+++ Java Substring.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int start  = sc.nextInt();
+        int end    = sc.nextInt();
+        sc.close();
+        
+        System.out.println(str.substring(start, end));
+    }
+}
