cee832a9fa2361e820231864dc220fa499c5edfa
StarKnight <92244026+StarKnightt@users.noreply.github.com>
2022-08-29 22:35:32 +0530

    Update Java String Reverse.java

--- Java String Reverse.java
+++ Java String Reverse.java
@@ -7,7 +7,6 @@ public class Solution {
         Scanner scan = new Scanner(System.in);
         String str = scan.nextLine();
         scan.close();
-        
         /* Reverse string and compare to original */
         String reversed = new StringBuilder(str).reverse().toString();
         System.out.println(str.equals(reversed) ? "Yes" : "No");
