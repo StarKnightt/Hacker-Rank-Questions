9276813df2f4ff65f2640ee4111dc9bc45bde1e3
StarKnight <92244026+StarKnightt@users.noreply.github.com>
2022-09-01 14:08:49 +0530

    Update Java String Tokens.java

--- Java String Tokens.java
+++ Java String Tokens.java
@@ -3,7 +3,7 @@ import java.util.*;
 import java.text.*;
 import java.math.*;
 import java.util.regex.*;
-
+// Java 7
 public class Solution {
     public static void main(String[] args) {
          Scanner sc = new Scanner(System.in);
