5e411b8d36706db833db3e883a8787f151495866
StarKnight <92244026+StarKnightt@users.noreply.github.com>
2022-09-02 22:50:35 +0530

    Update Pattern Syntax Checker.java

--- Pattern Syntax Checker.java
+++ Pattern Syntax Checker.java
@@ -1,1 +1,22 @@
+import java.util.Scanner;
+import java.util.regex.*;
+
+public class Solution
+{
+   public static void main(String[] args){
+      Scanner sc = new Scanner(System.in);
+      int Hello = Integer.parseInt(sc.nextLine());
+      while(Hello>0){
+         String pattern = sc.nextLine();
+          try{
+              Pattern p = Pattern.compile(pattern);
+              System.out.println("Valid");
+          }catch(Throwable t){
+              System.out.println("Invalid");
+          }
+          Hello--;
+             
+      }
+   }
+}
 
