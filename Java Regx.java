801e1550bc912263ea5e9c1fc7daf2fa6f044b07
StarKnight <92244026+StarKnightt@users.noreply.github.com>
2022-09-03 22:15:18 +0530

    Update Java Regx.java

--- Java Regx.java
+++ Java Regx.java
@@ -15,4 +15,4 @@ class MyRegex {
 String ip0to255 = "(\\d{1,2}||(0|1)\\d{2}||2[0-4]\\d||25[0-5])";
 public String pattern = ip0to255 + "\\." + ip0to255 + "\\." + ip0to255 + "\\." + ip0to255; 
 }
-
+// You are a good person.
