474a4151b726fc1aa8293da7aabd04eeb71d5083
StarKnight <92244026+StarKnightt@users.noreply.github.com>
2022-09-03 22:11:40 +0530

    Update Java Regx.java

--- Java Regx.java
+++ Java Regx.java
@@ -3,7 +3,6 @@ import java.util.regex.Pattern;
 import java.util.Scanner;
 // Java 7
 class Solution{
-
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         while(in.hasNext()){
