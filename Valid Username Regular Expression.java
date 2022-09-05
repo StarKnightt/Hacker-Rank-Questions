59658496c9337deb57cd91e12fc06759540f19af
StarKnight <92244026+StarKnightt@users.noreply.github.com>
2022-09-05 21:24:04 +0530

    Update Valid Username Regular Expression.java

--- Valid Username Regular Expression.java
+++ Valid Username Regular Expression.java
@@ -5,7 +5,6 @@ class UsernameValidator {
 }
 public class Solution {
     private static final Scanner scan = new Scanner(System.in);
-
     public static void main(String[] args) {
         int n = Integer.parseInt(scan.nextLine());
         while (n-- != 0) {
