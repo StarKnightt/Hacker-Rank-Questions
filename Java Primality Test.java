b9d97318d0df9cca0f92a8d2b18cd1932e4ab159
StarKnight <92244026+StarKnightt@users.noreply.github.com>
2022-09-08 10:09:09 +0530

    Update Java Primality Test.java

--- Java Primality Test.java
+++ Java Primality Test.java
@@ -9,7 +9,6 @@ import java.util.regex.*;
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-
         String n = bufferedReader.readLine();
         BigInteger bi = new BigInteger(n);
         System.out.println(bi.isProbablePrime(10) ? "prime" : "not prime");
