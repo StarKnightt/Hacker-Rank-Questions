8442724142add216a5f3f909703de6c9e6b811ff
StarKnight <92244026+StarKnightt@users.noreply.github.com>
2022-09-09 09:46:37 +0530

    Update Java BigInteger.java

--- Java BigInteger.java
+++ Java BigInteger.java
@@ -7,7 +7,6 @@ import java.util.regex.*;
 public class Solution {
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        
         Scanner userInput = new Scanner(System.in);
         BigInteger a;
         BigInteger b;
