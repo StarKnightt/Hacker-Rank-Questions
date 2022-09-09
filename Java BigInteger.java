587c1970f348116b747d525ff707da13db1948bd
StarKnight <92244026+StarKnightt@users.noreply.github.com>
2022-09-09 09:46:25 +0530

    Update Java BigInteger.java

--- Java BigInteger.java
+++ Java BigInteger.java
@@ -5,7 +5,6 @@ import java.math.*;
 import java.util.regex.*;
 // Java 15
 public class Solution {
-
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         
