0b80c4af1f11da5645e791db9d08c38f59a7b7a4
Prasenjit Nayak <92244026+StarKnightt@users.noreply.github.com>
2023-01-01 16:00:10 +0530

    Update Java Regx.java

--- Java Regx.java
+++ Java Regx.java
@@ -16,3 +16,4 @@ String ip0to255 = "(\\d{1,2}||(0|1)\\d{2}||2[0-4]\\d||25[0-5])";
 public String pattern = ip0to255 + "\\." + ip0to255 + "\\." + ip0to255 + "\\." + ip0to255; 
 }
 // You are a good person.
+
