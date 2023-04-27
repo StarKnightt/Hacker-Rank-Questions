47549714d3adaaef595853305c31f6247b203f3a
Prasenjit Nayak <92244026+StarKnightt@users.noreply.github.com>
2023-04-27 11:50:10 +0530

    Update Can You Access?.java
    Added comments on the program, so that it would be easy to understand

--- Can You Access?.java
+++ Can You Access?.java
@@ -6,29 +6,37 @@ import java.security.*;
 
 //Java 15
 public class Solution {
-
+    //Main method of the program
     public static void main(String[] args) throws Exception {
+        //Prevent program from terminating
         DoNotTerminate.forbidExit();    
-
         try{
+            //Create BufferedReader object to read input
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            //Read and parse integer input
             int num = Integer.parseInt(br.readLine().trim());
-            Object o;// Must be used to hold the reference of the instance of the class Solution.Inner.Private
+            //Create an instance of the Private class within the Inner class and assign to an object 'o'
+            Object o;
             Inner inner = new Inner();
             o = inner.new Private();
             Inner.Private innerPrivate = (Inner.Private) o;
+            //Call the powerof2 method with 'num' as input and store resulting string in 'response'
             String response = innerPrivate.powerof2(num);
+            //Print the input and result along with the canonical name of the 'o' object's class
             System.out.println(num + " is " + response);
-        System.out.println("An instance of class: " + o.getClass().getCanonicalName() + " has been created");
-        
+            System.out.println("An instance of class: " + o.getClass().getCanonicalName() + " has been created");
         }//end of try
-        
+        //Catch any ExitTrappedException thrown by the forbidExit method
         catch (DoNotTerminate.ExitTrappedException e) {
             System.out.println("Unsuccessful Termination!!");
         }
     }//end of main
+    
+    //Nested class within the Solution class
     static class Inner{
+        //Nested class within the Inner class
         private class Private{
+            //Method that checks if input number is a power of 2 and returns a string indicating the result
             private String powerof2(int num){
                 return ((num&num-1)==0)?"power of 2":"not a power of 2";
             }
@@ -36,13 +44,12 @@ public class Solution {
     }//end of Inner
 }//end of Solution
 
-class DoNotTerminate { //This class prevents exit(0)
-     
+//Class that prevents program from exiting
+class DoNotTerminate { 
     public static class ExitTrappedException extends SecurityException {
-
         private static final long serialVersionUID = 1L;
     }
- 
+    //Method to forbid program from exiting
     public static void forbidExit() {
         final SecurityManager securityManager = new SecurityManager() {
             @Override
@@ -55,4 +62,4 @@ class DoNotTerminate { //This class prevents exit(0)
         System.setSecurityManager(securityManager);
     }
 }   
-// Have a amazing day
+//End of program
