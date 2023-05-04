4151c6f6b7d1e7b3ef785fcbc6ddef00caf0a84f
Prasenjit Nayak <92244026+StarKnightt@users.noreply.github.com>
2023-05-04 20:31:19 +0530

    Update Java MD5.java
    Added some comments to it so that it would be easier to understand for everyone.

--- Java MD5.java
+++ Java MD5.java
@@ -1,29 +1,45 @@
+// Import statements for classes that are used in the code
 import java.io.*;
 import java.util.*;
 import java.security.MessageDigest;
 import java.util.Scanner;
 
-// Java 15
-
+// Define the public class that contains the main method
 public class Solution {
 
+    // Main method that is executed when the program is run
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+
+        // Create a new Scanner object to read input from standard input (keyboard)
         Scanner sc = new Scanner(System.in);
+
+        // Read a single string from the user and store it in the 'str' variable
         String str = sc.next();
+
+        // Close the scanner to free up system resources
         sc.close();
+
+        // Use a try-catch block to handle any exceptions that might be thrown
         try {
+
+            // Create a new MessageDigest object that uses the MD5 algorithm
             MessageDigest md = MessageDigest.getInstance("MD5");
+
+            // Update the MessageDigest object with the bytes of the input string
             md.update(str.getBytes());
-            // return bytesToHex(md.digest
+
+            // Get the resulting digest as a byte array
             byte[] digest = md.digest();
+
+            // Loop through the byte array and print each byte as a hexadecimal string
             for (byte b : digest) {
                 System.out.printf("%02x", b);
             }
+
+        // Catch any exceptions that might be thrown and re-throw them as a RuntimeException
         } catch (Exception ex) {
             throw new RuntimeException(ex);
         }
     }
 }
 
-// Have a fantastic day. 
