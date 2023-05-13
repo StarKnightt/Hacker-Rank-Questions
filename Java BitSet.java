36f5d1b2db8e4e13f344d84e167f0a78336740ec
Prasenjit Nayak <92244026+StarKnightt@users.noreply.github.com>
2023-05-13 23:00:09 +0530

    Update Java BitSet.java
    Added some comments in code, so that it would be easier to understand for evreyone.

--- Java BitSet.java
+++ Java BitSet.java
@@ -3,20 +3,30 @@ import java.util.*;
 import java.text.*;
 import java.math.*;
 import java.util.regex.*;
-// Java 15
+
+// Main class
 public class Solution {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
+        // Read input values
         int n = sc.nextInt();
+        
+        // Create two BitSets of size n
         BitSet[] a = new BitSet[] {null, new BitSet(n), new BitSet(n)};
+        
+        // Read number of operations to be performed
         int m = sc.nextInt();
         
+        // Loop through all operations
         for (int k = 0; k < m; k++) {
+            // Read operation type and BitSet indexes
             String cmd = sc.next();
             int k1 = sc.nextInt();
             int k2 = sc.nextInt();
+            
+            // Perform corresponding operation based on the given command
             if (cmd.equals("AND")) {
                 a[k1].and(a[k2]);
             } else if (cmd.equals("OR")) {
@@ -28,9 +38,11 @@ public class Solution {
             } else if (cmd.equals("SET")) {
                 a[k1].set(k2);
             }
+            
+            // Print the cardinality (number of set bits) of BitSets 1 and 2 after the operation
             System.out.println(a[1].cardinality() + " " + a[2].cardinality());
         }
     }
 }
 
-// Keep Hustling 
+// End of program. Keep Hustling!
